e7f78e3cf821a9e3d99aa531fbffe327b9b36daa
wendytao <wendytao@umich.edu>
2022-03-15 15:56:34 -0400

    create AR view but buggy

--- Radom/Radom/ARView.swift
+++ Radom/Radom/ARView.swift
@@ -6,13 +6,26 @@
 //
 
 import SwiftUI
+import RealityKit
 
 struct ARView: View {
     var body: some View {
-        Text("Hello, AR VIew!")
-            .padding()
+        ARViewContainer()
+            .edgesIgnoringSafeArea(.all)
     }
 }
+
+struct ARViewContainer: UIViewRepresentable {
+    func makeUIView(context: Context) -> ARView {
+        let arView = ARView(frame: .zero)
+        return arView
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {
+        //empty for now
+    }
+}
+
 
 struct ARView_Previews: PreviewProvider {
     static var previews: some View {
