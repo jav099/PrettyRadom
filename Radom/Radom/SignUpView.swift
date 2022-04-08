2c272ee6537a6863bd912a5affbbbea0acaf47b0
wendytao <wendytao@umich.edu>
2022-04-08 14:25:56 -0400

    create signup, not finish

--- Radom/Radom/SignUpView.swift
+++ Radom/Radom/SignUpView.swift
@@ -8,8 +8,57 @@
 import SwiftUI
 
 struct SignUpView: View {
+    @State var user = ""
+    @State var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            HStack{
+                VStack(alignment: .leading, spacing: 12){
+                    Text("Create an account")
+                        .font(.title)
+                        .fontWeight(.bold)
+                }
+                Spacer(minLength: 0)
+            }
+            .padding(.horizontal,25)
+            .padding(.top,30)
+            VStack(alignment: .leading, spacing: 15){
+                Text("Username")
+                    .font(.caption)
+                    .fontWeight(.bold)
+                    .foregroundColor(.gray)
+                TextField("username", text:$user)
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(5)
+                    .foregroundColor(.black)
+                
+                Text("Password")
+                    .font(.caption)
+                    .fontWeight(.bold)
+                    .foregroundColor(.gray)
+                TextField("password", text:$password)
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(5)
+                    .foregroundColor(.black)
+                    
+            }
+            .padding(.horizontal,25)
+            .padding(.top,25)
+            
+            // change destination to log in!!
+            NavigationLink(destination: TabMenuView()) {
+                Text("Sign Up")
+                .frame(minWidth: 0, maxWidth: 300)
+                .padding()
+                .foregroundColor(.white)
+                .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing))
+                .cornerRadius(40)
+                .font(.title)
+            }
+        }
     }
 }
 
